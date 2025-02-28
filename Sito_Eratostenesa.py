6109f10fa071d7f9dbdb23e0877acf1e4f26c39a
Emilia3d <emilia3d.pulawy@gmail.com>
2025-02-28 22:24:50 +0100

    poprwka

--- Sito_Eratostenesa.py
+++ Sito_Eratostenesa.py
@@ -8,16 +8,10 @@ print(pierwsze)
 d=2
 
 while d * d <= n:
-    i = d
-    while i * d <= n:
-        if i * d in pierwsze:
+    for i in range(d, (n // d) + 1):
+        if (i * d) in pierwsze:
             pierwsze.remove(i * d)
-        i += 1
 
-    # Znajdujemy najmniejszy element listy większy od d
-    for num in pierwsze:
-        if num > d:
-            d = num
-            break
+    d = min([num for num in pierwsze if num > d])
 
 print(pierwsze)
